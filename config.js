63844bc280414e0a543516e177e41ba6de5a1e56
wxnacy <371032668@qq.com>
2018-04-07 21:10:37 +0800

    修复bug

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ var CONFIG = {
   sidebar_file: "sidebar.md",
 
   // where the docs are actually stored on github - so you can edit
-  base_url: "https://github.com/vimhandbook/vimhandbook.github.io",
+  base_url: "https://github.com/vimhandbook/vimhandbook.github.io/blob/master",
 };
 
 // **************************
